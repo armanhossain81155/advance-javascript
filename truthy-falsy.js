4090be5807abae91af7ee90ed1a6508bb1fc0ea4
Arman Hossain <arman81155@gmail.com>
2021-04-30 21:42:34 +0600

    some code send

---
+++ truthy-falsy.js
@@ -0,0 +1,17 @@
+//Falsy 
+// 0
+// null
+//""
+// unddefined
+//NaN
+
+//Truthy
+//' ', '0', []
+
+let name = "Asif";
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
